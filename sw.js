d2137164bdde59ba5873a68f745e3d8b038b0d54
Anton Komarov <komanton@gmail.com>
2024-06-28 07:04:45 +0000

    Fix width for description block

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Updating PWA: https://stackoverflow.com/questions/66330440/how-to-check-for-installed-web-app-pwa-updates-when-using-precache-method
 
 // Files to cache
-const cacheName = 'pay-on-way-v4';
+const cacheName = 'pay-on-way-v5';
 const appShellFiles = [
   '/index.html',
   '/app.js',
